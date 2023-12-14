361ebe6de866716e0c87523b0100b6ef8164d5fe
James Moon <jwoo21century@gmail.com>
2023-12-13 23:09:01 -0800

    Implement basic 1-to-n chat functionality

--- src/client/client.c
+++ src/client/client.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,36 +8,68 @@
 
 #define PORT 8080
 
+pthread_t reader_thread, writer_thread;
+int socket_fd;
+
+void* reader(void* args) {
+    char receive_buffer[1024];
+    while (1) {
+        if (!read(socket_fd, receive_buffer, 1024)) {
+            break;
+        }
+        printf("%s\n", receive_buffer);
+    }
+    pthread_exit(NULL);
+}
+
+void* writer(void* args) {
+    char write_buffer[1000];
+    while (1) {
+        fgets(write_buffer, 1000, stdin);
+        write(socket_fd, write_buffer, strlen(write_buffer) + 1);
+    }
+    pthread_exit(NULL);
+}
+
 int main(int argc, char* argv[]) {
-  int status = 0;
-  struct sockaddr_in server_address;
-  char write_buffer[1024];
-  char receive_buffer[1024];
-  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int status = 0;
+    struct sockaddr_in server_address;
+    // Username with maximum of 20 characters + null character.
+    char username[21];
 
-  if (socket_fd < 0) {
-    perror("Socket creation failed");
-    exit(EXIT_FAILURE);
-  }
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(PORT);
-  server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-  if ((status = connect(socket_fd, 
-    (struct sockaddr *) &server_address, sizeof(server_address))) < 0) {
-    perror("Could not connect to the server");
-    exit(EXIT_FAILURE);
-  }
+    if ((status = connect(socket_fd, 
+        (struct sockaddr *) &server_address, sizeof(server_address))) < 0) {
+        perror("Could not connect to the server");
+        exit(EXIT_FAILURE);
+    }
 
-  while (1) {
-    fgets(write_buffer, 1024, stdin);
-    write(socket_fd, write_buffer, strlen(write_buffer) + 1);
-    read(socket_fd, receive_buffer, 1023);
-    printf("Message from the server: %s\n", receive_buffer);
-  }
-  
-  close(socket_fd);
+    printf("Successfully connected to the server.\n\n");
 
-  return 0;
+    // Prompt for the username and write it to the server.
+    printf("Enter your username: ");
+    fgets(username, 21, stdin);
+    size_t last = strlen(username) - 1;
+    if (username[last] == '\n')
+        username[last] = '\0';
+    write(socket_fd, username, strlen(username) + 1);
+
+    pthread_create(&reader_thread, NULL, reader, NULL);
+    pthread_create(&writer_thread, NULL, writer, NULL);
+
+    pthread_join(reader_thread, NULL);
+    pthread_join(writer_thread, NULL);
+
+    close(socket_fd);
+
+    return 0;
 }
--- src/server/server.c
+++ src/server/server.c
@@ -9,77 +9,103 @@
 
 #define PORT 8080
 #define MAX_CLIENT_COUNT 10
+#define MAX_TEXT_SIZE 1000
+#define MAX_USERNAME_SIZE 20
+#define MAX_RESPONSE_SIZE MAX_USERNAME_SIZE + MAX_TEXT_SIZE + 10
 
-pthread_t client_threads[MAX_CLIENT_COUNT];
+struct client {
+    pthread_t thread;
+    int socket_fd;
+    char name[MAX_USERNAME_SIZE];
+};
 
+struct client clients[MAX_CLIENT_COUNT];
 int client_count = 0;
 
-void* handle_client(void* params) {
-  char buffer[1024] = {0};
-  int bytes_read = 0;
-  int socket_fd = *((int *) params);
+void* handle_client(void* args) {
+    char buffer[MAX_TEXT_SIZE] = {0};
+    char response[MAX_RESPONSE_SIZE];
+    int bytes_read = 0;
+    struct client* client_info = (struct client *) args;
+    int socket_fd = client_info->socket_fd;
 
-  while (1) {
-    bytes_read = read(socket_fd, buffer, 1023);
-    buffer[bytes_read] = '\0';
+    // client joined
+    // Read client username.
+    bytes_read = read(socket_fd, buffer, 21);
+    printf("%s has connected to the server.\n", buffer);
+    strncpy(client_info->name, buffer, 21);
 
-    printf("From a client: %s\n", buffer);
-    send(socket_fd, buffer, strlen(buffer) + 1, 0);
-  }
+    while (1) {
+        if ((bytes_read = read(socket_fd, buffer, MAX_TEXT_SIZE)) == 0) {
+            break;
+        }
 
-  close(socket_fd);
-  pthread_exit(NULL);
+        printf("From client %s: %s\n", client_info->name, buffer);
+        snprintf(response, MAX_RESPONSE_SIZE, "%s: %s", client_info->name, buffer);
+
+        // Either concatenate the strings to make the write() atomic,
+        // or use a lock to call write() twice in a critical section.
+
+        for (int i = 0; i < client_count; i++) { 
+            write(clients[i].socket_fd, response, strlen(response) + 1);
+        }
+    }
+
+    // client disconnected
+    printf("%s has disconnected from the server.\n", client_info->name);
+
+    close(socket_fd);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char* argv[]) {
-  int server_fd, new_socket;
-  struct sockaddr_in address;
-  socklen_t addrlen = sizeof(address);
-  int opt = 1;
-  ssize_t valread;
-  int tids[MAX_CLIENT_COUNT];
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    int opt = 1;
+    ssize_t valread;
 
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("Failed to create socket");
-    exit(EXIT_FAILURE);
-  }
-
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-    perror("setsockopt() failed");
-    exit(EXIT_FAILURE);
-  }
-
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-
-  if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-    perror("Failed to bind server socket");
-    exit(EXIT_FAILURE);
-  }
-
-  if (listen(server_fd, 10) < 0) {
-    perror("Failed to listen for connections");
-    exit(EXIT_FAILURE);
-  }
-
-  while (client_count < MAX_CLIENT_COUNT) {
-    if ((tids[client_count] 
-      = accept(server_fd, (struct sockaddr *) &address, &addrlen)) < 0) {
-      perror("accept");
-      exit(EXIT_FAILURE);
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Failed to create socket");
+        exit(EXIT_FAILURE);
     }
 
-    pthread_create(&client_threads[client_count], NULL, handle_client, 
-      (void *) &tids[client_count]);
-    client_count++;
-  }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt() failed");
+        exit(EXIT_FAILURE);
+    }
 
-  for (int i = 0; i < client_count; i++) {
-    pthread_join(client_threads[i], NULL);
-  }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-  close(server_fd);
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("Failed to bind server socket");
+        exit(EXIT_FAILURE);
+    }
 
-  return 0;
+    if (listen(server_fd, MAX_CLIENT_COUNT) < 0) {
+        perror("Failed to listen for connections");
+        exit(EXIT_FAILURE);
+    }
+
+    while (client_count < MAX_CLIENT_COUNT) {
+        if ((clients[client_count].socket_fd 
+            = accept(server_fd, (struct sockaddr *) &address, &addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_create(&clients[client_count].thread, NULL, handle_client, 
+            (void *) &clients[client_count]);
+        client_count++;
+    }
+
+    for (int i = 0; i < client_count; i++) {
+        pthread_join(clients[i].thread, NULL);
+    }
+
+    close(server_fd);
+
+    return 0;
 }
